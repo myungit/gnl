c24588b753036c1542de135121b838a4bc327f97
Myung Park kim <mpark-ki@c2r14s1.42madrid.com>
2019-11-26 19:14:49 +0100

    hola.txt

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mpark-ki <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 22:29:38 by mpark-ki          #+#    #+#             */
-/*   Updated: 2019/11/26 18:59:55 by mpark-ki         ###   ########.fr       */
+/*   Updated: 2019/11/26 19:12:19 by mpark-ki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,24 +36,28 @@ int		get_next_line(int fd, char **line)
 		headlst = give_me_head(fd, buff, headlst);
 		while (buff[i] != '\n' && buff[i])
 		{
-			buff[i] = *line[i];
+			buff[i] = line[0][i];
+			printf("%c", line[0][i]);
 			i++;
 		}
-		*line[i] = 0;
+		line[0][i] = 0;
 		buff[i] = 0;
-		line++;
 	}
 	if (!line)
 		return (0);
-	while (line)
-		printf("%s\n", *line++);
+	i = 0;
+	while (line[0][i])
+		printf("%c\n", line[0][i++]);
 	return (1);
 }
-
+#include <fcntl.h>
 int main(void)
 {
-	char **hi = NULL;
+	char **hi = malloc(sizeof(char*) * 500);
+	*hi = malloc(sizeof(char) * 500);
+	int fd;
 
 	printf("%d\n", BUFFER_SIZE);
-	printf("%d\n", get_next_line(3, hi));
+	fd = open("hola.txt", O_RDONLY);
+	get_next_line(fd, hi);
 }
